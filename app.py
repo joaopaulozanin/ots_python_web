75b469c1057df53d5e47cd7602bc4bfe3329d310
João Tavares <joao.zanin@outlook.com.br>
2023-09-05 01:12:33 +0000

    Rota e HTML da página principal

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask("meu app")
+  
+posts =[
+    {
+        "titulo": "Minha primeira postagem",
+        "texto": "teste"
+    },
+    {
+        "titulo": "Segundo post",
+        "texto": "teste2"
+    }
+  ]  
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def exibir_entradas():
+    entradas = posts    #Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
+@app.route('/perfil')
+def exibir_perfil():
+    return render_template('exibir_perfil.html')
